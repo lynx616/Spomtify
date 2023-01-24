ab079dd0dced24fcceae86d0d7226ebd54246524
Rudra Karmakar <rudrakarmakar666@gmail.com>
2023-01-25 02:25:22 +0530

    carddata update

--- src/MusicCardData.jsx
+++ src/MusicCardData.jsx
@@ -1,138 +1,181 @@
 
-const ParentArray=[
-    
+const ParentArray = [
+
     {
         //cardata 1 starting
-    CardData:[{
-        image:"	https://i.scdn.co/image/ab67706f00000002712a998366ce62f36d690a9b",
-        hyperlink:"https://open.spotify.com/playlist/37i9dQZF1DWVq1SXCH6uFn",
-        heading:"00's Romance Hits",
-        paragraph:"A decade of Bollywood romance from early..."
+        CardData: [{
+            image: "	https://i.scdn.co/image/ab67706f00000002712a998366ce62f36d690a9b",
+            hyperlink: "https://open.spotify.com/playlist/37i9dQZF1DWVq1SXCH6uFn",
+            heading: "00's Romance Hits",
+            paragraph: "A decade of Bollywood romance from early..."
+        },
+        {
+            image: "https://i.scdn.co/image/ab67706f00000002374702be458b4953cc1f20c7",
+            hyperlink: "https://open.spotify.com/playlist/37i9dQZF1DX14CbVHtvHRB",
+            heading: "Trending Valentine's",
+            paragraph: "India's Ultimate Love Playlist. Cover..."
+        },
+        {
+            image: "https://i.scdn.co/image/ab67706f0000000231317ab3c48fee271f06b5c5",
+            hyperlink: "https://open.spotify.com/playlist/37i9dQZF1DX6q19gm5UQXx",
+            heading: "60's Romance Hits",
+            paragraph: "Best of Romance from the swinging 60's..."
+        },
+        {
+            image: "	https://i.scdn.co/image/ab67706f000000026caf8c414e0b49f1b5be3e91",
+            hyperlink: "https://open.spotify.com/playlist/37i9dQZF1DXdSavJjIP6Fb",
+            heading: "Bollywood R&B",
+            paragraph: "Hottest Bollywood R&B tunes!"
+        },
+        {
+            image: "	https://thisis-images.scdn.co/37i9dQZF1DZ06evO2uagOI-default.jpg",
+            hyperlink: "https://open.spotify.com/playlist/37i9dQZF1DZ06evO2uagOI",
+            heading: "This is KK",
+            paragraph: "Best of KK by Spotify..."
+        },
+        {
+            image: "https://thisis-images.scdn.co/37i9dQZF1DZ06evO2cV1FC-default.jpg",
+            hyperlink: "https://open.spotify.com/playlist/37i9dQZF1DZ06evO2cV1FC",
+            heading: "This is Joji",
+            paragraph: "The essential track, all in one playlist..."
+        }],
+        ///card data 1 ending
+        listheading: "Random Playlists"
     },
-    {
-        image:"https://i.scdn.co/image/ab67706f00000002374702be458b4953cc1f20c7",
-        hyperlink:"https://open.spotify.com/playlist/37i9dQZF1DX14CbVHtvHRB",
-        heading:"Trending Valentine's",
-        paragraph:"India's Ultimate Love Playlist. Cover..."
-    },
-    {
-        image:"https://i.scdn.co/image/ab67706f0000000231317ab3c48fee271f06b5c5",
-        hyperlink:"https://open.spotify.com/playlist/37i9dQZF1DX6q19gm5UQXx",
-        heading:"60's Romance Hits",
-        paragraph:"Best of Romance from the swinging 60's..."
-    },
-    {
-        image:"	https://i.scdn.co/image/ab67706f000000026caf8c414e0b49f1b5be3e91",
-        hyperlink:"https://open.spotify.com/playlist/37i9dQZF1DXdSavJjIP6Fb",
-        heading:"Bollywood R&B",
-        paragraph:"Hottest Bollywood R&B tunes!"
-    },
-    {
-        image:"	https://thisis-images.scdn.co/37i9dQZF1DZ06evO2uagOI-default.jpg",
-        hyperlink:"https://open.spotify.com/playlist/37i9dQZF1DZ06evO2uagOI",
-        heading:"This is KK",
-        paragraph:"Best of KK by Spotify..."
-    },
-    {
-        image:"https://thisis-images.scdn.co/37i9dQZF1DZ06evO2cV1FC-default.jpg",
-        hyperlink:"https://open.spotify.com/playlist/37i9dQZF1DZ06evO2cV1FC",
-        heading:"This is Joji",
-        paragraph:"The essential track, all in one playlist..."
-    }],
-    ///card data 1 ending
-    listheading:"Random Playlists"
-    },
-    
+
     {
         //carddta 2 starting
-    CardData:[
-        {
-            image:"	https://i.scdn.co/image/ab67706f00000002283997d725e0fdaa1ef91881",
-            hyperlink:"https://open.spotify.com/playlist/37i9dQZF1DX7KWRIlkUpsl",
-            heading:"This is Maroon 5",
-            paragraph:"Enjoy evry moments with Maroon 5!"
-        },
-        {
-            image:"		https://seeded-session-images.scdn.co/v1/img/artist/6eUKZXaKkcviH0Ku9w2n3V/en",
-            hyperlink:"https://open.spotify.com/playlist/37i9dQZF1E4msk0TFgsyTF",
-            heading:"Ed Sheeran Radio",
-            paragraph:"With Calum Scott, Sam Feldt..."
-        },
-        {
-            image:"	https://i.scdn.co/image/ab67616d00001e026b915e407b70e121e06fe979",
-            hyperlink:"https://open.spotify.com/album/01sfgrNbnnPUEyz6GZYlt9",
-            heading:"Du Lipa-Hits",
-            paragraph:"Best of Du Lipa..."
-        },
-        {
-            image:"	https://i.scdn.co/image/ab67616d00001e02a3b39c1651a617bb09800fd8",
-            hyperlink:"https://open.spotify.com/playlist/37i9dQZF1DXdSavJjIP6Fb",
-            heading:"CHARLIE",
-            paragraph:"2022.Charli Puth"
-        },
-        {
-            image:"	https://i.scdn.co/image/ab67706f00000002b7d25e59b0f3847ac0a66412",
-            hyperlink:"https://open.spotify.com/playlist/37i9dQZF1DWXTgXFpmvjDS",
-            heading:"This is ZAYN",
-            paragraph:"By Spotify"
-        },
-        {
-            image:"https://seeded-session-images.scdn.co/v1/img/artist/0EmeFodog0BfCgMzAIvKQp/en",
-            hyperlink:"https://open.spotify.com/playlist/37i9dQZF1E4zlbgA3fUmLK",
-            heading:"Shakira Radio",
-            paragraph:"With Rihanna, Calvin Harris, Katy Perry..."
-        }
-        
+        CardData: [
+            {
+                image: "	https://i.scdn.co/image/ab67706f00000002283997d725e0fdaa1ef91881",
+                hyperlink: "https://open.spotify.com/playlist/37i9dQZF1DX7KWRIlkUpsl",
+                heading: "This is Maroon 5",
+                paragraph: "Enjoy evry moments with Maroon 5!"
+            },
+            {
+                image: "		https://seeded-session-images.scdn.co/v1/img/artist/6eUKZXaKkcviH0Ku9w2n3V/en",
+                hyperlink: "https://open.spotify.com/playlist/37i9dQZF1E4msk0TFgsyTF",
+                heading: "Ed Sheeran Radio",
+                paragraph: "With Calum Scott, Sam Feldt..."
+            },
+            {
+                image: "	https://i.scdn.co/image/ab67616d00001e026b915e407b70e121e06fe979",
+                hyperlink: "https://open.spotify.com/album/01sfgrNbnnPUEyz6GZYlt9",
+                heading: "Du Lipa-Hits",
+                paragraph: "Best of Du Lipa..."
+            },
+            {
+                image: "	https://i.scdn.co/image/ab67616d00001e02a3b39c1651a617bb09800fd8",
+                hyperlink: "https://open.spotify.com/playlist/37i9dQZF1DXdSavJjIP6Fb",
+                heading: "CHARLIE",
+                paragraph: "2022.Charli Puth"
+            },
+            {
+                image: "	https://i.scdn.co/image/ab67706f00000002b7d25e59b0f3847ac0a66412",
+                hyperlink: "https://open.spotify.com/playlist/37i9dQZF1DWXTgXFpmvjDS",
+                heading: "This is ZAYN",
+                paragraph: "By Spotify"
+            },
+            {
+                image: "https://seeded-session-images.scdn.co/v1/img/artist/0EmeFodog0BfCgMzAIvKQp/en",
+                hyperlink: "https://open.spotify.com/playlist/37i9dQZF1E4zlbgA3fUmLK",
+                heading: "Shakira Radio",
+                paragraph: "With Rihanna, Calvin Harris, Katy Perry..."
+            }
+
         ],
         ///card data 2 ending
-    listheading:"Hollywood Playlists"
+        listheading: "Hollywood Playlists"
     },
-    
+
     {
         //carddta 3 starting
-    CardData:[
-        {
-            image:"	https://i.scdn.co/image/ab67616d00001e020acb5a72549287bf33b51b71",
-            hyperlink:"https://open.spotify.com/album/6Baavsh6HfNka1IyvPrr24",
-            heading:"Zindagi Nah Milegi Do...",
-            paragraph:"2011.ZNMD"
-        },
-        {
-            image:"https://i.scdn.co/image/ab67616d00001e02ae319e2da9a2e81dfe888409",
-            hyperlink:"https://open.spotify.com/album/79uqwO6n4ZFqHaDwLZhyL0",
-            heading:"Kites",
-            paragraph:"2010.Rajesh Roshan"
-        },
-        {
-            image:"	https://i.scdn.co/image/ab67616d00001e02707ea5b8023ac77d31756ed4",
-            hyperlink:"https://open.spotify.com/album/2Lxoc72vRTGdQfMvj7Ovi1",
-            heading:"Ye Jawaani Hai D...",
-            paragraph:"2010.Pritam"
-        },
-        {
-            image:"https://thisis-images.scdn.co/37i9dQZF1DZ06evO3oVu3D-default.jpg",
-            hyperlink:"https://open.spotify.com/playlist/37i9dQZF1DZ06evO3oVu3D",
-            heading:"This is Anupam Roy",
-            paragraph:"By Spotify"
-        },
-        {
-            image:"	https://i.scdn.co/image/ab67616d00001e022ce40efd9c6c7b89520e5486",
-            hyperlink:"https://open.spotify.com/playlist/37i9dQZF1DZ06evO2uagOI",
-            heading:"Dwitiyo Purush",
-            paragraph:"2020.Anupam Roy"
-        },
-        {
-            image:"	https://i.scdn.co/image/ab67616d00001e022ad8d5cc3c0e3c03719a636f",
-            hyperlink:"https://open.spotify.com/album/6JzPRHDXP5soq9zjamRB3f",
-            heading:"3 Idiots",
-            paragraph:"2009.Shantanu Moitra"
-        }
-        
+        CardData: [
+            {
+                image: "	https://i.scdn.co/image/ab67616d00001e020acb5a72549287bf33b51b71",
+                hyperlink: "https://open.spotify.com/album/6Baavsh6HfNka1IyvPrr24",
+                heading: "Zindagi Nah Milegi Do...",
+                paragraph: "2011.ZNMD"
+            },
+            {
+                image: "https://i.scdn.co/image/ab67616d00001e02ae319e2da9a2e81dfe888409",
+                hyperlink: "https://open.spotify.com/album/79uqwO6n4ZFqHaDwLZhyL0",
+                heading: "Kites",
+                paragraph: "2010.Rajesh Roshan"
+            },
+            {
+                image: "	https://i.scdn.co/image/ab67616d00001e02707ea5b8023ac77d31756ed4",
+                hyperlink: "https://open.spotify.com/album/2Lxoc72vRTGdQfMvj7Ovi1",
+                heading: "Ye Jawaani Hai D...",
+                paragraph: "2010.Pritam"
+            },
+            {
+                image: "https://thisis-images.scdn.co/37i9dQZF1DZ06evO3oVu3D-default.jpg",
+                hyperlink: "https://open.spotify.com/playlist/37i9dQZF1DZ06evO3oVu3D",
+                heading: "This is Anupam Roy",
+                paragraph: "By Spotify"
+            },
+            {
+                image: "	https://i.scdn.co/image/ab67616d00001e022ce40efd9c6c7b89520e5486",
+                hyperlink: "https://open.spotify.com/playlist/37i9dQZF1DZ06evO2uagOI",
+                heading: "Dwitiyo Purush",
+                paragraph: "2020.Anupam Roy"
+            },
+            {
+                image: "	https://i.scdn.co/image/ab67616d00001e022ad8d5cc3c0e3c03719a636f",
+                hyperlink: "https://open.spotify.com/album/6JzPRHDXP5soq9zjamRB3f",
+                heading: "3 Idiots",
+                paragraph: "2009.Shantanu Moitra"
+            }
+
         ],
         ///card data 3 ending
-    listheading:"Mood"
-    }  ,
-    
+        listheading: "Mood"
+    },
+    {
+        //carddta 4 starting
+        CardData: [
+            {
+                image: "https://i.scdn.co/image/ab67616d0000b2739c9e33853edda4d0c5c76b58",
+                hyperlink: "https://open.spotify.com/artist/1OxPcVkWgpIFAFBBl88jjG",
+                heading: "Tamakpata",
+                paragraph: "By Ashes"
+            },
+            {
+                image: "	https://i.scdn.co/image/ab67616d0000b27356e5ada7b21a4dd60f6a2672",
+                hyperlink: "https://open.spotify.com/track/2BeAciRWFn1sH6A6d4a4Wv?si=d91f2b540b6447a9",
+                heading: "Ei Srabon",
+                paragraph: "22 e Srabon"
+            },
+            {
+                image: "	https://i.scdn.co/image/ab67616d0000b2738f19db7788d42ae0b299593c",
+                hyperlink: "https://open.spotify.com/track/6tgpiYYPHmTVDTNbsjGFMJ?si=da8a0671cb304ae5",
+                heading: "Beche Thakar Gaan",
+                paragraph: "Rupam Islam"
+            },
+            {
+                image: "	https://i.scdn.co/image/ab67616d0000b273e9f1805aaf85d2371d8d321a",
+                hyperlink: "https://open.spotify.com/track/5hHK3dVpSg4WXkiA2BbyjC?si=21a311dc3bfb479e",
+                heading: "Tumi Robe Nirobe",
+                paragraph: "By SANAM"
+            },
+            {
+                image: "	https://i.scdn.co/image/ab67616d0000b273a310e191a88b9ffeacf8e559",
+                hyperlink: "https://open.spotify.com/track/5HnryFATFtrwSCfaxobKpB?si=906eb88b20764b2e",
+                heading: "Ei Toh Jibon",
+                paragraph: "Kishore Kumar"
+            },
+            {
+                image: "	https://i.scdn.co/image/ab67616d0000b273da537ea1f838985c50610ec5",
+                hyperlink: "https://open.spotify.com/track/3hobiW5AYC7GHNsjxVHwaC?si=7f4c8fc286384ee0",
+                heading: "2441139 - Bela Bose",
+                paragraph: "Anjan Dutt"
+            }
+
+        ],
+        ///card data 3 ending
+        listheading: "Bengali Hits"
+    }
 ];
 // const CardData1=[
 // {
@@ -211,7 +254,7 @@ const ParentArray=[
 //         heading:"Shakira-ALL SONGS",
 //         paragraph:"By Lynx"
 //     }
-    
+
 //     ];
 
 // const CardData3=[
@@ -251,7 +294,7 @@ const ParentArray=[
 //             heading:"3 Idiots",
 //             paragraph:"2009.Shantanu Moitra"
 //         }
-        
+
 //         ] ;   
 export default ParentArray;
 // export {CardData1,CardData2,CardData3};
